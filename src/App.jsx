d65ae5d3c477ccfadd615de222c2d067caf66876
Danylo Velizhanskyi <velizhanskiy1994@gmail.com>
2022-12-21 19:24:53 +0100

    basic styles, dark/light mode, components Header&Search are created

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Controls } from "./components/Controls";
+import { Header } from "./components/Header";
+import { Main } from "./components/Main";
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main>
+        <Controls />
+      </Main>
+    </>
+  );
+}
+
+export default App;
