9c0eb3856884d5720749ea3440e283f5f1aa8ad5
Danylo Velizhanskyi <velizhanskiy1994@gmail.com>
2022-12-28 15:07:04 +0100

    added Redux, themeSlice, custom hook useTheme to switch light/dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
-import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { IoMoon, IoMoonOutline } from 'react-icons/io5'
 
 import { Container } from './Container';
+import { ThemeSwitcher } from '../features/theme/ThemeSwitcher';
 
 const HeaderEl = styled.header`
     box-shadow: var(--shadow);
@@ -26,37 +25,16 @@ const Title = styled(Link).attrs({
     font-weight: var(--fw-bold);
 `;
 
-const ModeSwitcher = styled.div`
-    color: var(--colors-text);
-    font-size: var(--fs-md);
-    cursor: pointer;
-    /* font-weight: var(--fw-bold); */
-    text-transform: capitalize;
-`;
+
 
 export const Header = () => {
-
-    const [theme, setTheme] = useState('light');
-
-    const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
-
-    useEffect(() => {
-        document.body.setAttribute('data-theme', theme)
-    }, [theme]);
 
     return (
         <HeaderEl>
             <Container>
                 <Wrapper>
                     <Title>Where is the world?</Title>
-                    <ModeSwitcher onClick={toggleTheme}>
-                        {theme === 'light' ? (
-                            <IoMoonOutline size="14px" />
-                        ) : (
-                            <IoMoon size="14px" />
-                        )}
-                        <span style={{ marginLeft: '0.75rem' }}>{theme} Theme</span>
-                    </ModeSwitcher>
+                    <ThemeSwitcher />
                 </Wrapper>
             </Container>
         </HeaderEl>
